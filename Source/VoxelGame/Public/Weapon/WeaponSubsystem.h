5df1660c0a86968cc221bbccc46dd22cb4ea8a83
mateusz-gapinski <matgap478@gmail.com>
2025-12-10 15:02:47 +0100

    Cobble Material and MF_Burned
    Enhancement of CobbleMaterial and creating Material Function Burned for burned effect on surfaces.

---
+++ Source/VoxelGame/Public/Weapon/WeaponSubsystem.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/Subsystem.h"
+#include "Engine/Texture2D.h"
+#include "WeaponSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class VOXELGAME_API UWeaponSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UWeaponSubsystem();
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	UFUNCTION(BlueprintCallable, Category = "WeaponSystem")
+	//UTexture2D* Initialize(UTexture2D* BurnMap);
+	UTexture2D* Initialize(UTexture2D* BurnMap, int32 MapWidth, int32 MapLength, int32 MapHeight);
+	UFUNCTION(BlueprintCallable, Category = "WeaponSystem")
+	void ProcessCone(FVector Direction, float Radius, FVector BeginPosition, float Angle, float Value);
+	UFUNCTION(BlueprintCallable, Category = "WeaponSystem")
+	void UpdateTexture(bool bFreeData = false);
+
+private:
+
+	UPROPERTY()
+	UTexture2D* BurnsMap;
+	int32 Width;
+	int32 Length;
+	int32 Height;
+	
+};
